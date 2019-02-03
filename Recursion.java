371a837b5cb89913ee3d81e478a3b3fcb10becf2
mmiah <mmiah00@stuy.edu>
2019-02-02 22:49:21 -0500

    wrote fib constructor w helper function, started working on makeallsums but it didnt work so i deleted it

--- Recursion.java
+++ Recursion.java
@@ -1,18 +1,18 @@
 public class Recursion {
-  public static double mysqrt (double n, double guess) {
+  public static double mysqrt (double n, double guess, double tolerance) {
     if (n == 0) {
       return 0;
     }
-    if (percentDifference (guess, n) > .00001) {
-      return mysqrt (n,((n/guess + guess) / 2));
+    if (percentDifference (guess, n) > tolerance) {
+      return mysqrt (n,((n/guess + guess) / 2), tolerance);
     }
     else {
       return guess;
     }
   }
 
-  public static double sqrt (double n) {
-    return mysqrt (n, 1);
+  public static double sqrt (double n, double tolerance) {
+    return mysqrt (n, 1, tolerance);
   }
 
   public static double percentDifference (double guess, double square) {
@@ -20,10 +20,36 @@ public class Recursion {
     return ((Math.abs (mysquare - square)) / square) * 100;
   }
 
+  public static int fib (int n) {
+    return myfib (n,n);
+  }
+
+  public static int myfib (int count, int n) {
+    if (count < 2) {
+      return count;
+    }
+    else {
+      if (count - 2 < n) {
+        return myfib (count - 2,n) + myfib (count - 1, n);
+      }
+      else {
+        return myfib (count, n);
+      }
+    }
+  }
+
+  public static ArrayList<Integer> makeAllSums(int n){
+  }
+
   public static void main (String[] args) {
-    System.out.println ("Number" + "\t" + "Real" + "\tMine");
-    for (int x = 0; x < 21; x ++) {
-      System.out.println ( (x * x) + "\t" + x + "\t" + (sqrt (x * x)));
+    System.out.println ("Square Root:\nNumber" + "\t" + "Real" + "\tMine");
+    for (int x = 0; x < 26; x ++) {
+      System.out.println ( (x * x) + "\t" + x + "\t" + (sqrt ((x * x), .0001)));
+    }
+
+    System.out.println ("\nFibonacci:\nNumber\tSequence");
+    for (int x = 0; x < 26; x ++) {
+      System.out.println (x + "\t" + fib (x));
     }
   }
 }
